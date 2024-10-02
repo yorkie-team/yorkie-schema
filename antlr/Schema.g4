16d689f21a8cb3f38fe1b29d5e031c87f3b709d3
Smith <125195487+sigmaith@users.noreply.github.com>
2024-10-02 15:05:13 +0900

    Add test code

--- antlr/Schema.g4
+++ antlr/Schema.g4
@@ -1,46 +1,55 @@
 grammar Schema;
 
+// Lexer rules
+IDENTIFIER: [a-zA-Z_] [a-zA-Z0-9_]*;
+WHITESPACE: [ \t\n\r]+ -> skip;
+COMMENT: ('//' | '#') ~[\r\n]* -> skip;
+DIGIT: [0-9];
+
+// Keywords
+YORKIE_JSON_ARRAY: 'yorkie.JSONArray';
+YORKIE_JSON_OBJECT: 'yorkie.JSONObject';
+YORKIE_COUNTER: 'yorkie.Counter';
+YORKIE_TEXT: 'yorkie.Text';
+YORKIE_TREE: 'yorkie.Tree';
+
+// Operations and Symbols
+MINUS: '-';
+
 // Entry point of the grammar
 start: typeDefinitions EOF;
-typeDefinitions
-  : typeDefinition (WHITESPACE* typeDefinitions)?
-  ;
-typeDefinition
-  : 'type' IDENTIFIER '{' fieldList '}'
-  ;
-fieldList
-  : field (WHITESPACE* fieldList)?
-  ;
-field
-  : IDENTIFIER WHITESPACE* ':' WHITESPACE* fieldType
-  ;
-fieldType
-  : typeExpression arraySuffix*
-  ;
-typeExpression
-  : unionType
-  | simpleType
-  ;
-simpleType
-  : primitiveType
-  | IDENTIFIER
-  ;
-arraySuffix
-  : '[]'
-  ;
-unionType
-  : '(' WHITESPACE* unionTypeInner WHITESPACE* ')'
-  | unionTypeInner
-  ;
-unionTypeInner
-  : simpleType (WHITESPACE* '|' WHITESPACE* simpleType)*
-  ;
-primitiveType
-  : ('string' | 'number' | 'boolean')
-  ;
-IDENTIFIER
-  : [a-zA-Z_] [a-zA-Z0-9_]*
-  ;
-WHITESPACE
-  : [ \t\n\r]+ -> skip
-  ;
+typeDefinitions: typeDefinition (WHITESPACE* typeDefinitions)?;
+typeDefinition: 'type' IDENTIFIER '{' fieldList '}';
+fieldList: field (WHITESPACE* fieldList)?;
+field: IDENTIFIER WHITESPACE* ':' WHITESPACE* fieldType;
+fieldType: typeExpression arraySuffix*;
+typeExpression: unionType | simpleType;
+simpleType: primitiveType | literalType | IDENTIFIER;
+arraySuffix: '[]';
+unionType:
+	'(' WHITESPACE* unionTypeInner WHITESPACE* ')'
+	| unionTypeInner;
+unionTypeInner:
+	simpleType (WHITESPACE* '|' WHITESPACE* simpleType)*;
+primitiveType:
+	'string'
+	| 'number'
+	| 'boolean'
+	| 'null'
+	| YORKIE_JSON_ARRAY
+	| YORKIE_JSON_OBJECT
+	| YORKIE_COUNTER
+	| YORKIE_TEXT
+	| YORKIE_TREE;
+literalType:
+	booleanLiteralType
+	| numberLiteralType
+	| stringLiteralType;
+booleanLiteralType: 'true' | 'false';
+numberLiteralType: MINUS? DIGIT+ ('.' DIGIT+)?;
+stringLiteralType: DOUBLE_QUOTED_STRING | SINGLE_QUOTED_STRING;
+
+DOUBLE_QUOTED_STRING: '"' (ESC | ~["\n])* '"';
+
+SINGLE_QUOTED_STRING: '\'' (ESC | ~['\n])* '\'';
+fragment ESC: '\\' [btnr\\'"];
--- test/schema.test.ts
+++ test/schema.test.ts
@@ -26,3 +26,172 @@ describe("Schema", () => {
     );
   });
 });
+
+describe("Schema", () => {
+  it("should parse a valid schema with basic types", () => {
+    const schema = `
+      type Storage {    
+        todos: Todo[]
+      }
+      type Todo {
+        title: string
+        completed: boolean
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+  it("should parse a schema using Yorkie's basic types with comma", () => {
+    const schema = `
+      type Storage {
+        data: yorkie.JSONObject
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+  it("should parse a schema using Yorkie's basic types", () => {
+    const schema = `
+      type Storage {
+        data: yorkieJSONObject
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+  it("should parse a schema using Yorkie's complex types with comma", () => {
+    const schema = `
+      type Storage {
+        todos: yorkie.JSONArray
+        counter: yorkie.Counter
+        text: yorkie.Text
+        tree: yorkie.Tree
+      }
+    `;
+    assert.isTrue(validate(schema));
+  }); // fail
+
+  it("should parse a schema using Yorkie's complex types", () => {
+    const schema = `
+      type Storage {
+        todos: yorkieJSONArray
+        counter: yorkieCounter
+        text: yorkieText
+        tree: yorkieTree
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+  it("should not parse a schema with undefined types", () => {
+    const schema = `
+      type Storage {
+        unknownType: Hello
+      }
+    `;
+    assert.isFalse(validate(schema));
+  }); // fail
+
+  it("should parse a schema with Yorkie's primitive types", () => {
+    const schema = `
+      type Storage {
+        nullValue: null
+        booleanValue: boolean
+        numberValue: number
+        longValue: long
+        stringValue: string
+        bytesValue: bytes
+        dateValue: Date
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+  it("should not parse a schema with invalid syntax", () => {
+    const schema = `
+      type Storage {
+        invalidField: 
+      }
+    `;
+    assert.isFalse(validate(schema));
+  }); // fail
+
+  it("should not parse a schema with invalid syntax2", () => {
+    const schema = `
+      type Storage {
+        invalidField , boolean
+      }
+    `;
+    assert.isFalse(validate(schema));
+  });
+
+  it("should not parse a schema with invalid syntax3", () => {
+    const schema = `
+      type Storage {    
+        todos: Todo[]
+      }
+      type Todo {
+        title: string,
+        completed: boolean
+      }
+    `;
+    assert.isFalse(validate(schema));
+  }); // fail
+
+  it("multiple type definitions", () => {
+    const schema = `
+      type MyTypeName {
+      apple: string
+      banana: number
+      cookie: boolean
+      dog: number[]
+      string_or_number: string | number
+      array_of_string_or_number: (string | number)[]
+      complex: string | number | boolean[]
+      array_of_complex: (string | number | boolean)[]
+    }
+
+    type AnotherMyType {
+      mytypes: MyTypeName[]
+    }
+
+    type UserDetail {
+      address: string
+      age: number
+      foo: string | number | boolean
+      bar: string[]
+    }
+
+    type Root {
+      name: string
+      detail: UserDetail
+    }
+    `;
+    assert.isTrue(validate(schema));
+  }); // fail
+
+  it("anonymous type definition", () => {
+    const schema = `
+      type Storage  {
+        scientist: { name: string, age: number }
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+
+
+  it("value restriction", () => {
+    const schema = `
+      type Document  {
+        theme: "light" | "dark"
+        history: Event[]
+      }
+      
+      type Event {
+        statusCode: 200 | 400
+        info: string
+      }
+    `;
+    assert.isTrue(validate(schema));
+  });
+});
