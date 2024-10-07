8fa6f22d1f0e1a20b8d2a90edffdd346e3b7bbf3
Yourim Cha <yourim.cha@navercorp.com>
2024-10-07 14:39:29 +0900

    Test liveblocks codemirror linter

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { EditorState } from "@codemirror/state";
+import { EditorView, lineNumbers } from "@codemirror/view";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { linter } from "./codemirror-language/index";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+new EditorView({
+  state: EditorState.create({
+    doc: "# Your Liveblocks schema here\ntype User {\n  name: String\n  age: Number\n}",
+    extensions: [lineNumbers(), linter],
+  }),
+  parent: document.getElementById("editor")!,
+});
